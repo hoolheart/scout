035912672a176e3ef5f92b94cb33a097812e1670
Edward Chou <prettyage.new@gmail.com>
2025-12-20 17:44:00 +0800

    Fix qt demo due to qtcreator created project

---
+++ qt/demo01/QtDemo01_fr.ts
@@ -0,0 +1,55 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <location filename="build/QtDemo01_autogen/include_Debug/ui_mainwindow.h" line="90"/>
+        <source>Qt Demo 01</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="21"/>
+        <location filename="build/QtDemo01_autogen/include_Debug/ui_mainwindow.h" line="96"/>
+        <source>Welcome to Qt Demo 01\nThis is a simple Qt 5 application built with CMake</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="31"/>
+        <location filename="build/QtDemo01_autogen/include_Debug/ui_mainwindow.h" line="97"/>
+        <source>Click Me!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="48"/>
+        <location filename="build/QtDemo01_autogen/include_Debug/ui_mainwindow.h" line="98"/>
+        <source>File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="54"/>
+        <location filename="build/QtDemo01_autogen/include_Debug/ui_mainwindow.h" line="99"/>
+        <source>Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="64"/>
+        <location filename="build/QtDemo01_autogen/include_Debug/ui_mainwindow.h" line="91"/>
+        <source>Exit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="67"/>
+        <location filename="build/QtDemo01_autogen/include_Debug/ui_mainwindow.h" line="93"/>
+        <source>Ctrl+Q</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="72"/>
+        <location filename="build/QtDemo01_autogen/include_Debug/ui_mainwindow.h" line="95"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
